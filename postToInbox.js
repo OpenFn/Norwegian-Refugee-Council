537f7068138a96f319b195a98b4010b92e47e27d
Taylor Downs <downs.taylor@gmail.com>
2018-07-06 17:26:00 +0000

    log state to verify return from previous job

--- postToInbox.js
+++ postToInbox.js
@@ -1,6 +1,10 @@
+alterState(state => {
+  console.log(state);
+  return state;
+})
 // For every prepared projectWithFinancials item in the data array,
 // post it to the OpenFn inbox.
-each(
-  dataPath('data[*]'),
-  post('...')
-);
+// each(
+//   dataPath('data[*]'),
+//   post('...')
+// );
