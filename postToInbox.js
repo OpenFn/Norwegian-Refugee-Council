c640d4d4ae3f1569e85acc8e0ef4d63b88fb0c06
Taylor Downs <downs.taylor@gmail.com>
2018-07-06 17:39:12 +0000

    post each item in data array to inbox

--- postToInbox.js
+++ postToInbox.js
@@ -1,10 +1,14 @@
 alterState(state => {
   console.log(state);
   return state;
-})
+});
+
 // For every prepared projectWithFinancials item in the data array,
 // post it to the OpenFn inbox.
-// each(
-//   dataPath('data[*]'),
-//   post('...')
-// );
+each(
+  dataPath('data[*]'),
+  post("https://www.openfn.org/inbox/a507d61b-7d3c-44a4-a637-56cc9eca504c", {
+    body: state.data,
+    headers: { "content-type": "json" },
+  });
+);
