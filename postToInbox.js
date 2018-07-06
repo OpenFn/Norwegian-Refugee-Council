fbc0ac1a3174832d2642ff103f2b792e8ccccc71
Taylor Downs <taylor@openfn.org>
2018-07-06 17:46:09 +0000

    taylor@openfn.org OpenFn Web Edit

--- postToInbox.js
+++ postToInbox.js
@@ -6,7 +6,7 @@ alterState(state => {
 // For every prepared projectWithFinancials item in the data array,
 // post it to the OpenFn inbox.
 each(
-  dataPath('data[*]'),
+  dataPath('[*]'),
   post("https://www.openfn.org/inbox/a507d61b-7d3c-44a4-a637-56cc9eca504c", {
     body: state.data,
     headers: { "content-type": "json" },
