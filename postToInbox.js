dad6e9e015f360f23b05757e5d6fd24c5fdee968
Taylor Downs <downs.taylor@gmail.com>
2018-07-06 19:07:53 +0000

    only one project per post

--- postToInbox.js
+++ postToInbox.js
@@ -6,5 +6,7 @@ alterState(state => {
 // NOTE: For every prepared projectWithFinancials item in the projects array,
 // post it to the OpenFn inbox for individual error handling.
 each(dataPath('projects[*]'),
-  post("", { body: state.data }
+  post("", { body: state => {
+    return state.data;
+  }}
 ));
