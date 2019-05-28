73ba625436162ed458da7c9c99ff1a49c9935c68
Taylor Downs <taylor@openfn.org>
2019-05-28 10:56:25 -0400

    Update postToInbox.js

--- postToInbox.js
+++ postToInbox.js
@@ -1,5 +1,6 @@
 // NOTE: For every prepared projectWithFinancials item in the projects array,
-// post it to the OpenFn inbox for individual error handling.
+// post it to the OpenFn inbox for individual error handling. Now linked to
+// both A-L and M-Z fetch jobs.
 each(dataPath('projects[*]'),
   post("", { body: state => {
     return state.data;
