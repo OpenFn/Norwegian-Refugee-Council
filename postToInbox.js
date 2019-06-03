5d475977f5ab90c9922306f13dc37dbd3f20ce9a
Taylor Downs <downs.taylor@gmail.com>
2019-06-03 16:37:06 +0100

    add debug line and chunk size to 1000 financials

--- postToInbox.js
+++ postToInbox.js
@@ -1,8 +1,31 @@
 // NOTE: For every prepared projectWithFinancials item in the projects array,
 // post it to the OpenFn inbox for individual error handling. Now linked to
 // both A-L and M-Z fetch jobs.
-each(dataPath('projects[*]'),
-  post("", { body: state => {
-    return state.data;
-  }}
-));
+each(
+  dataPath("projects[*]"),
+  alterState(state => {
+    const financials = state.data.financials;
+    console.log(
+      `Isolating ${state.data.field2}: project ${state.data.field1} with ${
+        financials.length
+      } financials.`
+    );
+
+    var i,
+      j,
+      temparray,
+      chunk = 1000;
+
+    for (i = 0, j = financials.length; i < j; i += chunk) {
+      temparray = financials.slice(i, i + chunk);
+      state.data.financials = temparray;
+      post("", {
+        body: state => {
+          return state.data;
+        }
+      })(state);
+    }
+
+    return state;
+  })
+);
