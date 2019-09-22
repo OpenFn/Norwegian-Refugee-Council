dea3967630334c40aab31460b3964cbf102f7a96
Taylor Downs <downs.taylor@gmail.com>
2019-09-22 17:14:51 +0100

    back to no delay

--- postToInbox.js
+++ postToInbox.js
@@ -1,34 +1,31 @@
 // NOTE: For every prepared projectWithFinancials item in the projects array,
 // post it to the OpenFn inbox for individual error handling. Now linked to
 // both A-L and M-Z fetch jobs.
-alterState(state => {
-  const interval = 1000;
-  var promise = Promise.resolve();
-  state.data.projects.forEach(p => {
-    promise = promise.then(() => {
-      const financials = p.financials;
-      console.log(
-        `Isolating ${p.field2}: project ${p.field1} with ${financials.length} financials.`
-      );
+each(
+  dataPath("projects[*]"),
+  alterState(state => {
+    const financials = state.data.financials;
+    console.log(
+      `Isolating ${state.data.field2}: project ${state.data.field1} with ${
+        financials.length
+      } financials.`
+    );
 
-      var i,
-        j,
-        temparray,
-        chunk = 1000;
+    var i,
+      j,
+      temparray,
+      chunk = 1000;
 
-      for (i = 0, j = financials.length; i < j; i += chunk) {
-        temparray = financials.slice(i, i + chunk);
-        state.data.financials = temparray;
-        post('', {
-          body: state => {
-            return state.data;
-          },
-        })(state);
-      }
-      return new Promise(function(resolve) {
-        setTimeout(resolve, interval);
-      });
-    });
-  });
-  return state;
-});
+    for (i = 0, j = financials.length; i < j; i += chunk) {
+      temparray = financials.slice(i, i + chunk);
+      state.data.financials = temparray;
+      post("", {
+        body: state => {
+          return state.data;
+        }
+      })(state);
+    }
+
+    return state;
+  })
+);
